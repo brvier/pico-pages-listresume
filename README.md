c6546b85eb6f13d30dafff4a21697d1dea6370c9
Benoît HERVIER <khertan@khertan.net>
2014-01-30 09:07:51 +0100

    First release

--- README.md
+++ README.md
@@ -1,4 +1,89 @@
-pico-pages-listresume
-=====================
+# Pico Pages List Resume
 
-A nested pages list with resume plugin for the stupidly simple &amp; blazing fast, flat file CMS Pico
+A nested pages list with resume plugin for the stupidly simple & blazing fast, flat file CMS [Pico](http://pico.dev7studios.com).
+This plugin is derivated from [Pico Page List Plugin](https://github.com/nliautaud/pico-pages-list)
+
+## Installation
+
+Copy `pico_pages_listresume.php` to the `plugins` directory of your Pico Project.
+
+## Usage
+
+Add a generated nested pages list in your *theme* by using the following Twig variable :
+
+	{{ pages_listresume }}
+
+You'll automatically get something like :
+
+* [A cool page]()
+  A small resume of the page 
+* [Sub-page is coming]()
+    A small resume of the page 
+	* [The choosen one]()
+	  A small resume of the page 
+	* category
+		* [A page]()
+		  A small resume of the page 
+* [untitled]()
+  A small resume of the page 
+
+Under the hood :
+
+```html
+<ul>
+	<li class="titled">
+		<a href="http://mysite.com/titled">A cool page</a>
+	</li>
+	<li class="foo-page is-parent">
+		<a href="http://mysite.com/foo-page">Sub-page is coming</a>
+		<small>A small resume of the page</small>
+		<ul>
+			<li class="child is-current">
+				<a href="http://mysite.com/foo-page/child">The choosen one</a>
+				<small>A small resume of the page</small>
+			</li>
+			<li class="category">
+				category
+				<small>A small resume of the page</small>
+				<ul>
+					<li class="bar">
+						<a href="http://mysite.com/category/bar">A page</a>
+						<small>A small resume of the page</small>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li class="untitled">
+		<a href="http://mysite.com/untitled">untitled</a>
+		<small>A small resume of the page</small>
+	</li>
+</ul>
+```
+
+## Features
+
+The plugin generate a clean nested html list, using links only if the page exists. The page title is used if possible.
+
+The lists items are defined by css classes allowing per-page or general manipulations :
+
+```css
+#nav .foo-page a {
+	/* access to a specific page link */
+}
+#nav .foo-page .child a {
+	/* access to a specific foo-page/child link */
+}
+#nav .is-current {
+	/* access to the current page item */
+}
+#nav .is-parent {
+	/* access to every parent item of the current one */
+}
+```
+
+To exclude pages from the generated list, add to Pico config the setting `hide_pages`, and separate paths with commas. Childs of a path will be excluded with their parent :
+
+```php
+$config['listresume_hide_pages'] = 'this/page,all/in/here/';
+```
---
+++ pico_pages_listresume.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * A nested pages list plugin for the
+ * stupidly simple & blazing fast, flat file CMS Pico.
+ *
+ * @author	
+ * @link	http://nliautaud.fr
+ * @link    http://pico.dev7studios.com
+ * @license http://opensource.org/licenses/MIT
+ */
+class Pico_Pages_ListResume
+{
+	private $pages;
+	private $pages_urls;
+	private $current_url;
+	private $base_url;
+	private $hide_list;
+
+
+	// Pico hooks ---------------
+
+	/**
+	 * Store the base url defined in Pico settings.
+	 */
+	public function config_loaded(&$settings)
+	{
+		$this->base_url = $settings['base_url'];
+		$this->hide_list = array_map('trim', explode(',', $settings['listresume_hide_pages']));
+	}
+
+	/**
+	 * Store existing Pico pages urls, the current url
+	 * and construct the nested pages array.
+	 */
+	public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
+	{
+		$this->pages_urls = array();
+		foreach ($pages as $p) {
+			$this->pages_urls[] = $p['url'];
+		}
+
+		$this->pages = array();
+		$this->current_url = $current_page['url'];
+		$this->construct_pages($pages);
+	}
+
+	/**
+	 * Register the html output in the Twig {{ pages_list }} variable.
+	 */
+	public function before_render(&$twig_vars, &$twig)
+	{
+		$twig_vars['pages_listresume'] = $this->output($this->pages);
+	}
+
+
+	// CORE ---------------
+
+	/**
+	 * Create a nested array of the pages, according to their paths.
+	 * Merge all individual pages *nested_path*.
+	 *
+	 * @see    nested_path
+	 * @param  array $pages Pico pages flat array
+	 */
+	private function construct_pages($pages)
+	{
+		array_multisort($this->pages_urls, SORT_ASC, $pages);
+		foreach ($pages as $page)
+		{
+			$page['path'] = rtrim(str_replace($this->base_url.'/','',$page['url']), '/');
+			$nested_path = $this->nested_path($page);
+			$this->pages = array_merge_recursive($this->pages, $nested_path);
+		}
+	}
+
+	/**
+	 * Create a nested path of a given path, with page infos at the end.
+	 * Each path fragment is a "_child" of its parent fragment.
+	 *
+	 * @param  array  $page the corresponding page data, with 'path' key.
+	 * @return array        the nested path
+	 */
+	private function nested_path($page)
+	{
+		$parts = explode('/', $page['path']);
+		$count = count($parts);
+
+		$arr = array();
+		$parent = &$arr;
+		foreach($parts as $id => $part) {
+			$value = array();
+			if(!$part || $id == $count-1) {
+				$value = array(
+					'url'=>$page['url'],
+					'path'=>$page['path'],
+					'title'=>$page['title'],
+                    //'hide'=>$page['hide'],
+                    'excerpt'=>$page['excerpt']
+				);
+			}
+			if(!$part) {
+				$parent = $value;
+				break;
+			}
+			$parent['_childs'][$part] = $value;
+			$parent = &$parent['_childs'][$part];
+		}
+		return $arr;
+	}
+
+	/**
+	 * Create an html list based on the nested pages array.
+	 *
+	 * @param  array  $pages a nested pages array
+	 * @return string        the html list
+	 */
+	private function output($pages)
+	{
+		if(!isset($pages['_childs'])) return '';
+
+		$html = '<ul>';
+		foreach ($pages['_childs'] as $page)
+		{
+			//if (!array_key_exists('hide', $page)) continue;
+			
+			if (!array_key_exists('path', $page)) continue;
+			if($this->is_hidden($page['path'])) continue;
+			
+			$url = $page['url'];
+			$filename = basename($url);
+			$childs = $this->output($page);
+
+			// use title if the page have one, and make a link if the page exists.
+			$item = !empty($page['title']) ? $page['title'] : $filename;
+			if(in_array($url, $this->pages_urls))
+				$item = '<a href="'.$url.'">'.$item.'</a>';
+
+			// add the filename in class, and indicates if is current or parent
+			$class = $filename;
+			if($this->current_url == $url) $class .= ' is-current';
+			elseif(strpos($this->current_url, $url) === 0) $class .= ' is-parent';
+
+			$html .= '<li class="'.$class.'">' . $item . '<br /><small>'.$page['excerpt'].'</small>' . $childs . '</li>';
+		}
+		$html .= '</ul>';
+		return $html;
+	}
+
+	/**
+	 * Return if the given path had to be hidden or not.
+	 *
+	 * @param  string  $path the page short path
+	 * @return boolean
+	 */
+	private function is_hidden($path)
+	{
+		foreach($this->hide_list as $p)
+		{
+			if( !$p ) continue;
+			if( $path == $p ) return true;
+			if( strpos($path, $p) === 0 ) {
+				if( substr($p,-1) == '/' ) return true;
+				elseif( $path[strlen($p)] == '/' ) return true;
+			}
+		}
+		return false;
+	}
+}
+?>
